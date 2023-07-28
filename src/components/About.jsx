eece21a7dfa7c5d3861cc83f1cf51020bdb11f42
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-07-28 00:35:29 -0400

    start about me section, adding header, and about me text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>Introduction</p>
+				<h2 className={styles.sectionHeadText}>Overview.</h2>
+			</motion.div>
+
+			<motion.p 
+				variants={fadeIn("", "", 0.1, 1)}
+				className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+			>
+				I am Benjamin Morgiewicz, a software engineer who loves to write and fix code to find
+				a solution to any problem. I use my problem-solving brain and hard work
+				experience to make life easier and more fun with intelligent code.
+			</motion.p>
+
+
+
+		</>
+	)
 }
 
 export default About
