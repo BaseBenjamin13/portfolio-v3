0c8cc5f9e93f4957fe8ba8dbeca5473e96390589
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-08-01 19:21:27 -0400

    made testimonials section

--- src/constants/index.js
+++ src/constants/index.js
@@ -142,27 +142,11 @@ const experiences = [
 const testimonials = [
     {
         testimonial:
-            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-        name: "Sara Lee",
-        designation: "CFO",
-        company: "Acme Co",
-        image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-        testimonial:
-            "I've never met a web developer who truly cares about their clients' success like Rick does.",
-        name: "Chris Brown",
-        designation: "COO",
-        company: "DEF Corp",
-        image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-        testimonial:
-            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-        name: "Lisa Wang",
-        designation: "CTO",
-        company: "456 Enterprises",
-        image: "https://randomuser.me/api/portraits/women/6.jpg",
+            "Benjamin is a valuable team member and great to work alongside. He truly is invested in the project and is always willing to learn more and lend a hand. His love for problem solving and his proactive approach to finding solutions is a great asset and increases the efficiency of the team as a whole. Ben is a dependable full-stack software engineer and can have fun while doing it. He would be a great addition to any team!",
+        name: "Charnaye Grier",
+        designation: false,
+        company: false,
+        image: "https://avatars.githubusercontent.com/u/98440887?v=4",
     },
 ];
 
