67a4978ea7da75294406a1edd02491fa25d7f8d2
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-08-03 17:47:51 -0400

    updating testimonials

--- src/styles.js
+++ src/styles.js
@@ -3,6 +3,7 @@ const styles = {
   paddingX: "sm:px-16 px-6",
   paddingY: "sm:py-16 py-6",
   padding: "sm:px-16 px-6 sm:py-16 py-10",
+  paddingTestimonials: "sm:px-16 px-6 py-8",
 
   heroHeadText:
     "font-black text-white lg:text-[63px] sm:text-[34px] xs:text-[26px] text-[23px] lg:leading-[98px] mt-2",
